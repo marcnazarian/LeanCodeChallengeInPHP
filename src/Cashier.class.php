00972debe2bbd9f186adddc46f01129e9818468b
Marc Nazarian <marc.nazarian@gmail.com>
2012-02-10 23:28:19 +0100

    iteration 1

--- src/Cashier.class.php
+++ src/Cashier.class.php
@@ -2,11 +2,29 @@
 
 class Cashier {
     
+  var $total = 0;
+    
   public function checkout() {
     while (true) {
       $input = readline();
-      echo $input . PHP_EOL;
+      echo scanItemAndReturnTotal($input) . PHP_EOL;
     }
+  }
+  
+  public function scanItemAndReturnTotal($item) {
+      $this->total += $this->scanItem($item);
+      return $this->total;
+  }
+  
+  public function scanItem($item) {
+      if ($item == "Apples") {
+          return 100;
+      } elseif ($item == "Cherries") {
+          return 75;
+      } elseif ($item == "Bananas") {
+          return 150;
+      }
+      
   }
   
 }
--- test/CashierTest.php
+++ test/CashierTest.php
@@ -1,12 +1,33 @@
 <?php
 
-require_once dirname(__FILE__) . '/../Cashier.class.php';
+require_once dirname(__FILE__) . '/../src/Cashier.class.php';
 
 class CashierTest extends PHPUnit_Framework_TestCase {
 
     /** @test */
-    public function dummyTest() {
-        $this->assertTrue(true);
+    public function appleCost100() {
+        $cashier = new Cashier();
+        $this->assertEquals(100, $cashier->scanItem("Apples"));
     }
 
+    /** @test */
+    public function cherriesCost75() {
+        $cashier = new Cashier();
+        $this->assertEquals(75, $cashier->scanItem("Cherries"));
+    }
+    
+    /** @test */
+    public function bananasCost150() {
+        $cashier = new Cashier();
+        $this->assertEquals(150, $cashier->scanItem("Bananas"));
+    }
+    
+    /** @test */
+    public function printCartTotal() {
+        $cashier = new Cashier();
+        $this->assertEquals(100, $cashier->scanItemAndReturnTotal("Apples"));
+        $this->assertEquals(175, $cashier->scanItemAndReturnTotal("Cherries"));
+        $this->assertEquals(250, $cashier->scanItemAndReturnTotal("Cherries"));
+    }
+    
 }
